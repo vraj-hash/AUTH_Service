f2ab45eec0580e7de88e9fcfca746f9b355f1942
vraj-hash <202201328@daiict.ac.in>
2025-09-20 04:05:29 +0530

    Added get user by id without password

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ const app = express();
 
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true })); 
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+
   app.use("/api", apiRoutes);
+
+  
   app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`);
   });
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -24,6 +24,18 @@ class UserRepository {
       throw error;
     }
   }
+
+  async getById(userId) {
+    try {
+      const user = await User.findByPk(userId, {
+        attributes: ["email", "id"],
+      });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong on repository");
+      throw error;
+    }
+  }
 }
 
 module.exports = UserRepository;
