fe7b9ffd5da89a9a1c47933d43c7e84f01083fb2
vraj-hash <202201328@daiict.ac.in>
2025-09-20 13:59:40 +0530

    Added function to comapre the incomming password with encrypted password

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/server-config");
 const apiRoutes = require("./routes/index");
+ UserService = require("./services/user-service");
+
+
+UserService = require('./services/user-service');
+
+
 
 const app = express();
 
@@ -13,10 +19,24 @@ const prepareAndStartServer = () => {
 
   app.use("/api", apiRoutes);
 
-  
+
   app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`);
+     
+
+
+    // const service = new UserService();
+    // const newToken = service.createToken({email:' vraj@admin.com ', id:'1'});
+    // console.log("new token is",newToken);
+    
+    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IiB2cmFqQGFkbWluLmNvbSAiCJpZCI6IjEiLCJpYXQiOjE3NTgzMjI1NzgsImV4cCI6MTc1ODMyNjE3OH0.OPnJl_SRG28YI0IgLQL9aht2G3ceB82bhDGjHGXmE8U';
+    //  const response = service.verifyToken(token);
+    //  console.log(response);
+
   });
+
+
+  
 };
 
 prepareAndStartServer();
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,19 +1,50 @@
-const UserRepository = require('../repository/user-repository');
+const UserRepository = require("../repository/user-repository");
+const jwt = require("jsonwebtoken");
+const { JWT_KEY } = require("../config/server-config");
+const bcrypt = require("bcrypt");
 
-class UserService{
-    constructor(){
-        this.userRepository = new UserRepository();
+class UserService {
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+  async create(data) {
+    try {
+      const user = await this.userRepository.create(data);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in service ");
+      throw error;
     }
-    async create(data){
-        try{
-            const user = await this.userRepository.create(data);
-            return user;
-        } catch (error)
-        {
-            console.log("Something went wrong in service ");
-            throw error;
-        }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1h" });
+      return result;
+    } catch {
+      console.log("Something went wrong while creating the token");
+      throw error;
     }
+  }
+
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong while verifying the token", error);
+      throw error;
+    }
+  }
+
+  checkPassword(userInputPlainPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+    } catch {
+      console.log("Something went wrong while creating passwrod");
+      throw error;
+    }
+  }
 }
 
 module.exports = UserService;
